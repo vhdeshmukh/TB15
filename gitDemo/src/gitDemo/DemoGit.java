9ee5daeac3a6234a83a7cb49bf7d2330eafc6ca7
vhdeshmukh <107527649+vhdeshmukh@users.noreply.github.com>
2022-06-15 12:28:04 +0530

    by cjc

--- gitDemo/src/gitDemo/DemoGit.java
+++ gitDemo/src/gitDemo/DemoGit.java
@@ -1,6 +1,11 @@
 package gitDemo;
 
 public class DemoGit {
+	
+	public void m1()
+	{
+		System.out.println("m1----method")
+	}
 
 	public static void main(String[] args) {
 		System.out.println("main method");
